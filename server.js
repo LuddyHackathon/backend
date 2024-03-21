2153487beb29ea1da6645f043e694940d815fd10
Yash <yashshah035@gmail.com>
2024-03-21 18:17:50 +0530

    adding upload

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const http = require('http');
+const fileUpload = require('express-fileupload');
 
 const app = express();
 const port = 65535;
+
+var engines = require('consolidate');
+
+app.engine('html', engines.mustache);
+app.set('view engine', 'html');
+
+app.use(fileUpload());
 
 app.get('/', (req, res) => {
     res.set('Content-Type', 'text/html');
@@ -68,6 +76,31 @@ app.post('/grammar', (req, res) => {
 
     httpRequest.end();
 })
+
+app.get('/data', (req, res) => {
+    res.render('index.html',{status:'good'});
+})
+
+app.post('/data', function(req, res) {
+    let sampleFile;
+    let uploadPath;
+  
+    if (!req.files || Object.keys(req.files).length === 0) {
+      return res.status(400).send('No files were uploaded.');
+    }
+  
+    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    sampleFile = req.files.sampleFile;
+    uploadPath = __dirname + '/' + sampleFile.name;
+  
+    // Use the mv() method to place the file somewhere on your server
+    sampleFile.mv(uploadPath, function(err) {
+      if (err)
+        return res.status(500).send(err);
+  
+      res.send('File uploaded!');
+    });
+  });
 
 app.get('/recommender', (req, res) => {
     const options = {
