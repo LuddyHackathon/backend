2c867724bb0b25dbbc90e310a223add610fb2007
yashshah035 <yashshah035@gmail.com>
2024-05-24 19:28:04 +0530

    seperate folder to save voice data

--- routes/transcriber.js
+++ routes/transcriber.js
@@ -1,3 +1,5 @@
+import http from 'http';
+
 export function getTranscriber(req, res) {
     res.render('index.html', { status: 'good' });
 };
@@ -11,12 +13,39 @@ export function postTranscriber(req, res) {
     };
 
     voiceFile = req.files.voiceFile;
-    uploadPath = voiceFile.name;
+    uploadPath = '/voice/' + voiceFile.name;
 
     voiceFile.mv(uploadPath, function (err) {
         if (err) {
             return res.status(500).send(err);
         };
-        res.send('File uploaded!');
+        const options = {
+            hostname: 'transcriber',
+            port: 65535,
+            path: '/?file=' + encodeURIComponent(req.query.file),
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        };
+
+        const httpRequest = http.request(options, (response) => {
+            let responseData = '';
+
+            response.on('data', (chunk) => {
+                responseData += chunk;
+            });
+
+            response.on('end', () => {
+                res.send(responseData);
+            });
+        });
+
+        httpRequest.on('error', (error) => {
+            console.error('Error:', error);
+            res.status(500).json({ success: false, error: error.message });
+        });
+
+        httpRequest.end();
     });
 };
