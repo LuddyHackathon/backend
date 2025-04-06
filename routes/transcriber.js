00c1d1b31d80f358abdfbfffea39d9dd3541b7f6
Darshula <tanmayvijaykulkarni2002@gmail.com>
2025-04-06 09:14:07 -0400

    invoking transcriber immediately after upload

--- routes/transcriber.js
+++ routes/transcriber.js
@@ -5,18 +5,9 @@ export function getTranscriber(req, res) {
 };
 
 export function postTranscriber(req, res) {
-    let voiceFile;
-    let uploadPath;
-
-    if (!req.files || Object.keys(req.files).length === 0) {
-        return res.status(400).send('No files were uploaded.');
-    };
-
-    voiceFile = req.files.voiceFile;
-    uploadPath = '/voice/' + voiceFile.name;
+    let voiceFile = req.query.voiceFile;
 
     let transcribedText;
-    let paraphrasedText;
 
     voiceFile.mv(uploadPath, async function (err) {
         if (err) {
@@ -26,7 +17,7 @@ export function postTranscriber(req, res) {
         const transcriberOptions = {
             hostname: 'transcriber',
             port: 65535,
-            path: '/?voice_file=' + encodeURIComponent(voiceFile.name),
+            path: '/?voice_file=' + encodeURIComponent(voiceFile),
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -41,41 +32,14 @@ export function postTranscriber(req, res) {
 
             response.on('end', () => {
                 transcribedText = responseData;
-                console.log('transcriber output:', transcribedText);
-                const paraphraserOptions = {
-                    hostname: 'paraphraser',
-                    port: 65535,
-                    path: '/?text=' + encodeURIComponent(transcribedText),
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    }
-                };
-                const paraphraserHttpRequest = http.request(paraphraserOptions, (response) => {
-                    let responseData = '';
+                res.status(200).json({ 'file': voiceFile.name, 'transcribed': transcribedText });
 
-                    response.on('data', (chunk) => {
-                        responseData += chunk;
-                    });
-
-                    response.on('end', () => {
-                        paraphrasedText = responseData;
-                        paraphrasedText = paraphrasedText.substring(paraphrasedText.indexOf(' '), paraphrasedText.length)
-                        console.log('paraphraser output:', paraphrasedText);
-                        res.status(200).json({ 'file': voiceFile.name, 'transcribed': transcribedText, 'paraphrased': paraphrasedText });
-                    });
-                });
-                paraphraserHttpRequest.on('error', (error) => {
-                    console.error('Error:', error);
-                    res.status(500).json({ success: false, error: error.message });
-                });
-                paraphraserHttpRequest.end();
             });
+            transcriberHttpRequest.on('error', (error) => {
+                console.error('Error:', error);
+                res.status(500).json({ success: false, error: error.message });
+            });
+            transcriberHttpRequest.end();
         });
-        transcriberHttpRequest.on('error', (error) => {
-            console.error('Error:', error);
-            res.status(500).json({ success: false, error: error.message });
-        });
-        transcriberHttpRequest.end();
     });
 };
